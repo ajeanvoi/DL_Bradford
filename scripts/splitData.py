6519a019cb6cbf970c6899d8650c5343128a8cb1
Achille Jeanvoine <jeanvoine.achille@gmail.com>
2024-07-30 17:20:38 +0100

    modif

--- scripts/splitData.py
+++ scripts/splitData.py
@@ -4,12 +4,13 @@ from sklearn.model_selection import train_test_split
 
 def load_data(csv_path):
     """Load data from the CSV file."""
-    return pd.read_csv(csv_path, sep=" ", header=None, skiprows=1)
+    return pd.read_csv(csv_path, sep=" ", header=None, skiprows=1, on_bad_lines='skip')
+
 
 def split_data(data):
     """Split data into training, validation, and test sets."""
-    train_data, temp_data = train_test_split(data, test_size=0.4, random_state=42, shuffle=True, stratify=data['Classification'])
-    val_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=42, shuffle=True, stratify=temp_data['Classification'])
+    train_data, temp_data = train_test_split(data, test_size=0.4, random_state=42, shuffle=True)
+    val_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=42, shuffle=True)
     return train_data, val_data, test_data
 
 def rename_columns(data):
@@ -45,7 +46,6 @@ if __name__ == "__main__":
     data = load_data(csv_path)
     print(f"Loaded data with {len(data)} rows.")
 
-    data = rename_columns(data)
     train_data, val_data, test_data = split_data(data)
 
     print("Column names before renaming:")
@@ -58,12 +58,12 @@ if __name__ == "__main__":
     print("\nColumn names after renaming:")
     print(train_data.columns.tolist())
 
-    save_data(train_data, train_path, "train")
-    save_data(val_data, val_path, "validation")
-    save_data(test_data, test_path, "test")
+    save_data(train_data, train_path, "train_modif")
+    save_data(val_data, val_path, "validation_modif")
+    save_data(test_data, test_path, "test_modif")
 
     # Print the number of rows in each file
     print(f"\nNumber of rows in train set: {len(train_data)}")
     print(f"Number of rows in validation set: {len(val_data)}")
     print(f"Number of rows in test set: {len(test_data)}")
-    print("\nColumns renamed and data saved successfully on data/processed folder.")
+    print("\nColumns renamed and data saved successfully on data/rawSplit folder.")
